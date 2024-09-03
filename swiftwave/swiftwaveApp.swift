feec79aa8af8ee866c9dfcedf68ebe1893abe712
Gary Mejia <gmejima@ucsc.edu>
2024-09-02 18:48:13 -0700

    Base project

---
+++ swiftwave/swiftwaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftwaveApp.swift
+//  swiftwave
+//
+//  Created by Gary Mejia on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftwaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
